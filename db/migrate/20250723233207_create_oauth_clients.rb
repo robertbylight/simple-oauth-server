ec021f8d9517d6572a81ee2004f8b42217e71e00
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-07-24 09:52:53 -0400

    update schema for oauth_clients table; enforces unique client_id and no null values

--- db/migrate/20250723233207_create_oauth_clients.rb
+++ db/migrate/20250723233207_create_oauth_clients.rb
@@ -1,9 +1,9 @@
 class CreateOauthClients < ActiveRecord::Migration[7.2]
   def change
     create_table :oauth_clients do |t|
-      t.string :client_id
-      t.string :client_name
-      t.string :redirect_uri
+      t.string :client_id, null: false, index: { unique: true }
+      t.string :client_name, null: false
+      t.string :redirect_uri, null: false
 
       t.timestamps
     end

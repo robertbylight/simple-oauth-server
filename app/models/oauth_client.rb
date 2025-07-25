387dc1597ea49fd08a369bce169b0bdb30ce985b
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-07-25 09:44:31 -0400

    generate migration and model, update client model to validate client and create auth code with 10 min expiration time.

--- app/models/oauth_client.rb
+++ app/models/oauth_client.rb
@@ -1,2 +1,19 @@
 class OauthClient < ApplicationRecord
+  has_many :oauth_authorization_codes, dependent: :destroy
+
+  validates :client_id, presence: true, uniqueness: true
+  validates :client_name, presence: true
+  validates :redirect_uri, presence: true
+
+  def valid_redirect_uri?(uri)
+    redirect_uri == uri
+  end
+
+  def create_authorization_code!(redirect_uri)
+    oauth_authorization_codes.create!(
+      code: SecureRandom.hex(10),
+      redirect_uri: redirect_uri,
+      expires_at: 10.minutes.from_now
+    )
+  end
 end

3c546afc0098790b80f96f395907068b6bae1644
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-08-04 15:41:38 -0400

    upate controller to store auth client instances in a hash.

--- app/controllers/oauth/oauth_controller.rb
+++ app/controllers/oauth/oauth_controller.rb
@@ -1,13 +1,11 @@
 module Oauth
   class OauthController < ApplicationController
-    attr_accessor :client_id
     skip_before_action :verify_authenticity_token
 
     def authorize
-      self.client_id = params[:client_id]
       validate_authorization_request(params)
 
-      code = oauth_client.create_authorization_code!(params[:redirect_uri])
+      code = oauth_client(params[:client_id]).create_authorization_code!(params[:redirect_uri])
       redirect_url = build_redirect_url(params[:redirect_uri], { code: })
       render json: { redirect_url: }
     rescue ArgumentError => e
@@ -18,14 +16,15 @@ module Oauth
 
     def validate_authorization_request(params)
       raise ArgumentError, "Missing client_id" if params[:client_id].blank?
-      raise ArgumentError, "Invalid client_id" if oauth_client.nil?
+      raise ArgumentError, "Invalid client_id" if oauth_client(params[:client_id]).nil?
       raise ArgumentError, "response_type must be code" if params[:response_type] != "code"
       raise ArgumentError, "Missing redirect_uri" if params[:redirect_uri].blank?
-      raise ArgumentError, "Invalid redirect_uri" unless oauth_client.redirect_uri == params[:redirect_uri]
+      raise ArgumentError, "Invalid redirect_uri" unless oauth_client(params[:client_id]).redirect_uri == params[:redirect_uri]
     end
 
-    def oauth_client
-      @oauth_client ||= OauthClient.find_by(client_id:)
+    def oauth_client(client_id)
+      @oauth_client ||= {}
+      @oauth_client[client_id] ||= OauthClient.find_by(client_id:)
     end
 
     def build_redirect_url(base_uri, params)

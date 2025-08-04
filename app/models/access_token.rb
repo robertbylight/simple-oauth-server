ed5276eeeabcbce5841e8d4d241c0999b5cd3ad0
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-08-04 10:38:43 -0400

    add has many tokens association to user, update access token model with methods for generating a token, finding out if its expired, and retrieving all active tokens.

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,4 +1,20 @@
 class AccessToken < ApplicationRecord
   belongs_to :oauth_client
   belongs_to :user
+
+  validates :token, presence: true, uniqueness: true
+  validates :expires_at, presence: true
+
+  # for retrieving all active tokens
+  def self.active
+    where("expires_at > ?", Time.current)
+  end
+
+  def expired
+    expires_at < Time.current
+  end
+
+  def self.generate_token
+    SecureRandom.hex(32)
+  end
 end

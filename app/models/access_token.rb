61101a840c51df4a558895a4c44207beedb46936
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-08-07 11:25:59 -0400

    spec for access token

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -5,11 +5,6 @@ class AccessToken < ApplicationRecord
   validates :token, presence: true, uniqueness: true
   validates :expires_at, presence: true
 
-  # for retrieving all active tokens
-  def self.active
-    where("expires_at > ?", Time.current)
-  end
-
   def expired
     expires_at < Time.current
   end

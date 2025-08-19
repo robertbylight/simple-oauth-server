5c437a5fc34014bc8bb669798d9afff246be372b
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-08-19 15:20:39 -0400

    clean up pkce validator

--- app/services/pkce_validator.rb
+++ app/services/pkce_validator.rb
@@ -3,8 +3,9 @@ class PkceValidator
     return false if code_challenge.blank? || code_verifier.blank?
 
     generated_challenge = Base64.urlsafe_encode64(
-      Digest::SHA256.digest(code_verifier)
-    ).tr("=", "")
+      Digest::SHA256.digest(code_verifier),
+      padding: false
+    )
 
     generated_challenge == code_challenge
   end

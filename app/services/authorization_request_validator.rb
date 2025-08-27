c6627cdb0964e04c06d38aecae5c342a06d9b4f4
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-08-27 10:37:15 -0400

    checkout refactored services from code-exchange branch

---
+++ app/services/authorization_request_validator.rb
@@ -0,0 +1,52 @@
+class AuthorizationRequestValidator
+  attr_reader :client_id, :user_id, :response_type, :redirect_uri,
+              :code_challenge, :code_challenge_method
+
+  def initialize(params)
+    @client_id = params[:client_id]
+    @user_id = params[:user_id]
+    @response_type = params[:response_type]
+    @redirect_uri = params[:redirect_uri]
+    @code_challenge = params[:code_challenge]
+    @code_challenge_method = params[:code_challenge_method]
+  end
+
+  def validate
+    validate_required_params
+    validate_client_exists_and_matches
+    validate_response_type
+    validate_pkce_params
+  end
+
+  private
+
+  def validate_required_params
+    raise ArgumentError, "Missing client_id" if client_id.blank?
+    raise ArgumentError, "Missing redirect_uri" if redirect_uri.blank?
+    raise ArgumentError, "Missing user_id" if user_id.blank?
+  end
+
+  def validate_client_exists_and_matches
+    oauth_client = OauthClient.find_by(client_id:)
+    raise ArgumentError, "Invalid client_id" unless oauth_client
+
+    unless oauth_client.redirect_uri == redirect_uri
+      raise ArgumentError, "Invalid redirect_uri"
+    end
+  end
+
+  def validate_response_type
+    unless response_type == "code"
+      raise ArgumentError, "response_type must be code"
+    end
+  end
+
+  def validate_pkce_params
+    raise ArgumentError, "Missing code_challenge" if code_challenge.blank?
+    raise ArgumentError, "Missing code_challenge_method" if code_challenge_method.blank?
+
+    unless code_challenge_method == "S256"
+      raise ArgumentError, "Invalid code_challenge_method"
+    end
+  end
+end
---
+++ app/services/token_request_validator.rb
@@ -0,0 +1,37 @@
+class TokenRequestValidator
+  attr_reader :grant_type, :code, :client_id, :redirect_uri, :code_verifier
+
+  def initialize(params)
+    @grant_type = params[:grant_type]
+    @code = params[:code]
+    @client_id = params[:client_id]
+    @redirect_uri = params[:redirect_uri]
+    @code_verifier = params[:code_verifier]
+  end
+
+  def validate
+    validate_grant_type
+    validate_required_params
+    validate_client_exists
+  end
+
+  private
+
+  def validate_grant_type
+    unless grant_type == "authorization_code"
+      raise ArgumentError, "grant_type must be authorization_code"
+    end
+  end
+
+  def validate_required_params
+    raise ArgumentError, "Missing code" if code.blank?
+    raise ArgumentError, "Missing client_id" if client_id.blank?
+    raise ArgumentError, "Missing redirect_uri" if redirect_uri.blank?
+    raise ArgumentError, "Missing code_verifier" if code_verifier.blank?
+  end
+
+  def validate_client_exists
+    client = OauthClient.find_by(client_id:)
+    raise ArgumentError, "Invalid client_id" unless client
+  end
+end

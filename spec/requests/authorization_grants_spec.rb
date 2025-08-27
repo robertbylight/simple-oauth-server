299cc0c6714992adf3a1d735ef5bb53d714ac630
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-08-27 12:39:20 -0400

    remove client secret from auth grant spec

--- spec/requests/authorization_grants_spec.rb
+++ spec/requests/authorization_grants_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Oauth::AuthorizationGrantsController, type: :request do
       client_id: '123',
       client_name: 'Rubber Toes',
       redirect_uri: 'https://www.robert.com/callback',
-      client_secret: 'test_secret'
     )
   }
 
